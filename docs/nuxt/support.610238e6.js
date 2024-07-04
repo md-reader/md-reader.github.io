918e95666217a0cdafea8908decdd476475078e5
Bener <benjavan@163.com>
2024-07-04 09:10:05 +0000

    CI BUILD

---
+++ docs/nuxt/support.610238e6.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o as t,p as c,f as _,a}from"./entry.06b0f249.js";const n={},p=o=>(c("data-v-9ab21c4b"),o=o(),_(),o),d={class:"content"},r=p(()=>a("h1",{class:"text-3xl font-bold"},"Coming soon",-1)),i=[r];function l(o,f){return t(),s("div",d,i)}const b=e(n,[["render",l],["__scopeId","data-v-9ab21c4b"]]);export{b as default};
