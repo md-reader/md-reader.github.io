2df83f8acdeba89bdd319a5256fec0a8d7609442
Bener <benjavan@163.com>
2025-06-20 13:28:30 +0000

    CI BUILD

--- docs/nuxt/BAxRHYoE.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as s,a as t,o as c}from"./TVR2fIAg.js";const n={},a={class:"content"};function r(_,e){return c(),s("div",a,e[0]||(e[0]=[t("h1",{class:"text-3xl"},"Coming soon...",-1)]))}const l=o(n,[["render",r],["__scopeId","data-v-78be9595"]]);export{l as default};
